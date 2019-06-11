3e3691d7ccc3a7a68074347e937b69a9cc08d1b8
andrewdimaio <andrewdimaio777@gmail.com>
2019-06-11 18:35:33 -0400

    First Commit

---
+++ src/test/java/com/groupFour/HandTest.java
@@ -0,0 +1,18 @@
+package com.groupFour;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HandTest {
+
+    @Test
+    public void addCard() {
+
+    }
+
+    @Test
+    public void removeCard() {
+
+    }
+}
---
+++ src/test/java/com/groupFour/PlayerTest.java
@@ -0,0 +1,18 @@
+package com.groupFour;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+
+    }
+
+    @Test
+    public void setName() {
+
+    }
+}
