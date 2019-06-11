dcb88e476d03d0e86de5c314de646f6460fad3c0
donnasvinis <dvsvinis@mac.com>
2019-06-11 17:45:56 -0400

    Create  DeckTest

---
+++ src/test/java/com/groupFour/DeckTest.java
@@ -0,0 +1,28 @@
+package com.groupFour;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeckTest {
+
+    @Test
+    public void shuffle() {
+    }
+
+    @Test
+    public void getCard() {
+    }
+
+    @Test
+    public void putCard() {
+    }
+
+    @Test
+    public void removeCard() {
+    }
+
+    @Test
+    public void renewDraw() {
+    }
+}
