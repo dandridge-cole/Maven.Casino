e7f39664d202988cacd93a50910fad44c36c397b
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-11 17:26:07 -0400

    Updated project structure to match UML diagram

---
+++ src/main/java/com/groupFour/Player.java
@@ -0,0 +1,15 @@
+package com.groupFour;
+
+public class Player {
+
+    private String name;
+    private Double wallet;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
