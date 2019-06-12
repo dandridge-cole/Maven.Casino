2903b7347111915eb370721096f481c2a1f10f22
Henry <cataniahf@gmail.com>
2019-06-12 17:05:22 -0400

    fuck

--- src/main/java/com/groupFour/Dice.java
+++ src/main/java/com/groupFour/Dice.java
@@ -1,22 +1,32 @@
 package com.groupFour;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Dice {
     final private Integer numberOfDice;
     private ArrayList<Integer> diceResult;
 
-    public Dice(){
-        numberOfDice = 0;
+    public Dice(Integer numberOfDice) {
+        this.numberOfDice = numberOfDice;
     }
+
 
 
     public void rollDice(){
+        Random random = new Random();
+        Integer roll = random.nextInt(6)+ 1;
+
+        for (int i = 0; i < numberOfDice; i++) {
+            diceResult.add(roll);
+        }
     }
 
     public ArrayList<Integer> getDiceResult() {
-        return diceResult;
+
+        return null;
     }
 
 
 }
+
