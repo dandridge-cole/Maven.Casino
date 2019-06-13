0aafa169147eaab5c6d8fca573290cfdeabf6801
Henry <cataniahf@gmail.com>
2019-06-13 17:26:52 -0400

    Methods stubbed out in GreaterThanThree class GreaterThanThreeTest class added w/generated test stubbs

--- src/main/java/com/groupFour/Games/GreaterThanThree.java
+++ src/main/java/com/groupFour/Games/GreaterThanThree.java
@@ -4,39 +4,50 @@ import com.groupFour.Dice;
 import com.groupFour.Interfaces.Game;
 import com.groupFour.Player;
 
+import java.util.ArrayList;
+
 public class GreaterThanThree extends Game {
 
-    static final String gameName = "Greater than Three";
     private Dice dice;
     private Player player;
+    private ArrayList<Integer> finalList;
+    private Integer rollCount;
 
     public GreaterThanThree(Player player){
         this.player = player;
         this.dice=new Dice(5);
+        this.finalList = new ArrayList<Integer>();
     }
 
-    public GreaterThanThree(){this(new Player());}
-
-    public void reRoll(){
-
-    }
-    public void takeTurn() {
+    public GreaterThanThree(){
+        this(new Player());
 
     }
-
     public void setup() {
+        //intialize(roll) dice
+        //check for matches in diceResult
+        //rollCount++
+    }
+    public void reRoll(){
+        //create new dice(5-finalList.size)
+        //roll
+        //rollCount++
+        //Check if rollCount > 4
+        //if rollCount = 4, end game.
 
     }
 
-    public void placeBet(Double amount) {
-
+    public void takeTurn() {
+        //print diceResult ArrayList
+        //check if any diceResult elements = finalList(1)
+        //get player dice choice(if not just match)
+        //move choice to final list
+        //if finalList.size >= 3 ask if roll again
     }
 
-    public void resolve() {
-
-    }
-
-    public void validateBet() {
-
+    public void resolve(){
+        //get points(pts = finalList.size
+        //if finalList.size < 3 lose game
+        //if finalList.size >= 3 win game
     }
 }
---
+++ src/test/java/com/groupFour/Games/GreaterThanThreeTest.java
@@ -0,0 +1,24 @@
+package com.groupFour.Games;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreaterThanThreeTest {
+
+    @Test
+    public void setup() {
+    }
+
+    @Test
+    public void reRoll() {
+    }
+
+    @Test
+    public void takeTurn() {
+    }
+
+    @Test
+    public void resolve() {
+    }
+}
