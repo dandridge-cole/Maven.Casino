f48739473bdf8a9e3d0ff027ca082352cb3a4093
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-14 15:20:43 -0400

    Fixing tests for abstract classes, for all game constructors that now require console parameter

--- src/test/java/com/groupFour/Interfaces/GamblingGameTest.java
+++ src/test/java/com/groupFour/Interfaces/GamblingGameTest.java
@@ -1,18 +1,18 @@
 package com.groupFour.Interfaces;
 
+import com.groupFour.Console;
 import com.groupFour.Games.Blackjack;
 import com.groupFour.Games.Craps;
 import com.groupFour.Games.Spanish21;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GamblingGameTest {
+    Console console = new Console(System.in,System.out);
 
     @Test
     public void getMaxBet() {
-        Spanish21 s21Game = new Spanish21();
+        Spanish21 s21Game = new Spanish21(console);
         Double expected = 50.00;
         s21Game.setMaxBet(expected);
         Double actual = s21Game.getMaxBet();
@@ -21,7 +21,7 @@ public class GamblingGameTest {
 
     @Test
     public void setMaxBet() {
-        Spanish21 s21Game = new Spanish21();
+        Spanish21 s21Game = new Spanish21(console);
         s21Game.setMinBet(10.00);
         Double expected = 150.00;
         s21Game.setMaxBet(expected);
@@ -31,7 +31,7 @@ public class GamblingGameTest {
 
     @Test
     public void setMaxBetNegative() {
-        Craps crapGame = new Craps();
+        Craps crapGame = new Craps(console);
         Double invalid = -150.00;
         crapGame.setMaxBet(invalid);
         Double expected=null;
@@ -41,7 +41,7 @@ public class GamblingGameTest {
 
     @Test
     public void setMaxBetTooLow() { // can't allow maxBet < minBet
-        Blackjack bjGame = new Blackjack();
+        Blackjack bjGame = new Blackjack(console);
         Double expected = null;
  //       bjGame.setMaxBet(expected);
         bjGame.setMinBet(30.00);
@@ -53,7 +53,7 @@ public class GamblingGameTest {
 
     @Test
     public void getMinBet() {
-        Craps crapGame = new Craps();
+        Craps crapGame = new Craps(console);
         Double expected = 50.00;
         crapGame.setMinBet(expected);
         Double actual = crapGame.getMinBet();
@@ -62,7 +62,7 @@ public class GamblingGameTest {
 
     @Test
     public void setMinBet() {
-        Craps crapGame = new Craps();
+        Craps crapGame = new Craps(console);
         crapGame.setMaxBet(500.00);
         Double expected = 150.00;
         crapGame.setMinBet(expected);
@@ -72,7 +72,7 @@ public class GamblingGameTest {
 
     @Test
     public void setMinBetNegative() {
-        Blackjack bjGame = new Blackjack();
+        Blackjack bjGame = new Blackjack(console);
         bjGame.setMaxBet(500.00);
         Double invalid = -50.00;
         bjGame.setMinBet(invalid);
@@ -83,7 +83,7 @@ public class GamblingGameTest {
 
     @Test
     public void setMinBetTooHigh() { // can't allow maxBet < minBet
-        Blackjack bjGame = new Blackjack();
+        Blackjack bjGame = new Blackjack(console);
         bjGame.setMaxBet(5.00);
         Double invalid = 500.00;
         bjGame.setMinBet(invalid);
@@ -94,7 +94,7 @@ public class GamblingGameTest {
 
     @Test
     public void getCurrentBet() {
-        Blackjack bjGame = new Blackjack();
+        Blackjack bjGame = new Blackjack(console);
         bjGame.setMinBet(50.00);
         bjGame.setMaxBet(500.00);
         Double expected = 50.00;
@@ -105,7 +105,7 @@ public class GamblingGameTest {
 
     @Test
     public void setCurrentBet() {
-        Blackjack bjGame = new Blackjack();
+        Blackjack bjGame = new Blackjack(console);
         bjGame.setMinBet(50.00);
         bjGame.setMaxBet(500.00);
         Double expected = 150.00;
@@ -116,7 +116,7 @@ public class GamblingGameTest {
 
     @Test
     public void setCurrentBetNegative() {
-        Spanish21 s21Game = new Spanish21();
+        Spanish21 s21Game = new Spanish21(console);
         Double invalidBet = -10.00;
         Double expected = null;
         s21Game.setCurrentBet(invalidBet);
@@ -148,7 +148,7 @@ public class GamblingGameTest {
 */
     @Test
     public void setCurrentBetTooHigh() {
-        Spanish21 s21Game = new Spanish21();
+        Spanish21 s21Game = new Spanish21(console);
         s21Game.setMinBet(50.00);
         s21Game.setMaxBet(500.00);
         Double invalidBet = 1000.00;
@@ -160,7 +160,7 @@ public class GamblingGameTest {
 
     @Test
     public void setCurrentBetTooLow() {
-        Spanish21 s21Game = new Spanish21();
+        Spanish21 s21Game = new Spanish21(console);
         s21Game.setMinBet(50.00);
         s21Game.setMaxBet(500.00);
         Double invalidBet = 10.00;
