b6dcd8056fc8c6f703badfa9428ce72ef13e89fd
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-14 07:48:56 -0400

    Mediate, cleaned up code, added some cosmetic improvements to the console prompts, added some tests for invalid inputs

--- src/main/java/com/groupFour/Mediator.java
+++ src/main/java/com/groupFour/Mediator.java
@@ -12,22 +12,21 @@ class Mediator {
     private List<String> gameNames;
     private Boolean quit;
 
-    public Mediator(Console console) {
+    Mediator(Console console) {
         this.console = console;
         this.player = new Player();
         this.gameNames = new ArrayList<>();
         this.quit = false;
     }
 
-
-    public Mediator(Console console, Player player) {
+    Mediator(Console console, Player player) {
         this.console = console;
         this.player = player;
         this.gameNames = new ArrayList<>();
         this.quit = false;
     }
 
-    public Mediator() {
+    Mediator() {
         this.console = new Console(System.in,System.out);
         this.player = new Player();
         this.gameNames = new ArrayList<>();
@@ -43,12 +42,25 @@ class Mediator {
         }
     }
 
-    void initPlayer(Player player){
-        String name = console.getStringInput("Welcome to the best virtual casino ever!  And whom do we have the honor of fleecing today?");
+
+    /* *************
+     * INTIALIZERS *
+     * ************/
+
+    void initPlayer(Player player){// could be private except for testing
+        String welcomeMessage=
+                "*****                  *****\n" +
+                "****                    ****\n" +
+                "**         Casino         **\n" +
+                "**   Opulence  Vulgaire   **\n" +
+                "****                    ****\n" +
+                "*****                  *****\n" +
+                "\n\nWelcome to the best virtual casino ever!  And whom do we have the honor of fleecing today?";
+        String name = console.getStringInput(welcomeMessage);
         player.setName(name);
     }
 
-    void initPlayerWallet(Player player){
+    void initPlayerWallet(Player player){// could be private except for testing
         if(ALLOW_CUSTOM_WALLET) {
             Double amount = console.getDoubleInput("And how much may we exchange for you, to get started?");
             Double confirmedAmt = confirmValidWalletIncrease(amount, player);
@@ -57,12 +69,119 @@ class Mediator {
         else player.setWallet(Player.DEFAULT_WALLET);
     }
 
-    void initGamesList(){
+    void initGamesList(){ // could be private except for testing
         gameNames.add(Blackjack.gameName);
         gameNames.add(Craps.gameName);
         gameNames.add(GoFish.gameName);
         gameNames.add(GreaterThanThree.gameName);
         gameNames.add(Spanish21.gameName);
+    }
+
+    private Boolean keepTheCustomer() {
+        Boolean kept = true;
+        String answer = console.getStringInput("Could we offer you a complimentary drink?");
+        switch (answer.toLowerCase()) {
+            case "n":
+            case "no":
+            case "no thankyou":
+            case "no thank you": {
+                kept = false;
+                break;
+            }
+            default:
+                break;
+        }
+        //more time wasting could be added here;
+        return kept;
+    }
+    void increaseBalance(){
+        Double amount = (console.getDoubleInput("You currently have $%,1.2f. How much would you like to add?",player.getWallet()));
+        Double confirmedAmt = confirmValidWalletIncrease(amount, player);
+        player.setWallet(player.getWallet()+confirmedAmt);
+    }
+
+    Double confirmValidWalletIncrease(Double amount, Player player) {  // could be private except for testing
+        while(amount<=0||amount>999999999) {
+            if (amount <= 0) {
+                amount = console.getDoubleInput("I'm sorry, but we do not exchange negative or zero balances. Please enter a valid amount.");
+            }
+            else{
+                amount = console.getDoubleInput("I'm sorry, but we are not able to exchange billions in individual transactions. Please enter a reasonable amount.");
+            }
+        }
+        return amount;
+    }
+
+
+    /* ***********
+     * MAIN MENU *
+     * **********/
+
+    private void greetPlayer(){
+        console.println(String.format("Hello %s, you currently have $%,1.2f.",player.getName(),player.getWallet()));
+    }
+
+    private String buildMainMenu() {
+        Integer menuItem=0;
+        StringBuilder builder = new StringBuilder("\n" +
+                "****************\n" +
+                "* CASINO LOBBY *\n" +
+                "****************\n" +
+                "\nHow may we serve you today?");
+        builder.append(String.format("\n\t%d: Choose a game", menuItem++));
+        builder.append(String.format("\n\t%d: Increase Balance", menuItem++));
+        builder.append(String.format("\n\t%d: Reset Player", menuItem++));
+        builder.append(String.format("\n\t%d: Quit\n", menuItem));
+        return console.getStringInput(builder.toString());
+    }
+
+    private void mainMenu(){
+        boolean resetPlayer=false;
+        while(!quit&&!resetPlayer){
+            greetPlayer();
+            String selection = buildMainMenu();
+            switch (selection.toLowerCase()) {
+                case "0":
+                case "choose a game":{
+                    startGame();
+                    break;
+                }
+                case "1":
+                case "increase balance":{
+                    increaseBalance();
+                    break;
+                }
+                case "2":
+                case "reset player":{
+                    resetPlayer=true;
+                    break;
+                }
+                case "3":
+                case "quit":{
+                    if(!keepTheCustomer()) quit=true;
+                    break;
+                }
+                default: break;
+            }
+        }
+    }
+
+
+    /* ************
+     * GAMES MENU *
+     * ***********/
+
+    String buildGamesMenu() { // could be private except for testing
+        StringBuilder builder = new StringBuilder("\n" +
+                "****************\n" +
+                "* GAMING FLOOR *\n" +
+                "****************\n" +
+                "\nPlease select a game:");
+        for (int i = 0; i < gameNames.size(); i++) {
+            builder.append(String.format("\n\t%d: %S", i, gameNames.get(i)));
+        }
+        builder.append(String.format("\n\t%d: Previous Menu\n", gameNames.size()));
+        return console.getStringInput(builder.toString());
     }
 
     private void startGame(){
@@ -110,96 +229,5 @@ class Mediator {
                 }
             }
         }
-    }
-
-    String buildGamesMenu() {
-        StringBuilder builder = new StringBuilder("Please select a game:");
-        for (int i = 0; i < gameNames.size(); i++) {
-            builder.append(String.format("\n\t%d: %S", i, gameNames.get(i)));
-        }
-        builder.append(String.format("\n\t%d: Previous Menu\n", gameNames.size()));
-        return console.getStringInput(builder.toString());
-    }
-
-
-    private void greetPlayer(){
-        console.println(String.format("Hello %s, you currently have $%,1.2f.",player.getName(),player.getWallet()));
-    }
-
-    private void mainMenu(){
-        boolean resetPlayer=false;
-        while(!quit&&!resetPlayer){
-            Integer menuItem=0;
-            greetPlayer();
-            String selection = buildMainMenu();
-            switch (selection.toLowerCase()) {
-                case "0":
-                case "choose a game":{
-                    startGame();
-                    break;
-                }
-                case "1":
-                case "increase balance":{
-                    increaseBalance();
-                    break;
-                }
-                case "2":
-                case "reset player":{
-                    resetPlayer=true;
-                    break;
-                }
-                case "3":
-                case "quit":{
-                    if(!keepTheCustomer()) quit=true;
-                    break;
-                }
-                default: break;
-            }
-        }
-    }
-
-    private String buildMainMenu() {
-        Integer menuItem=0;
-        StringBuilder builder = new StringBuilder("What would you like to do?");
-        builder.append(String.format("\n\t%d: Choose a game", menuItem++));
-        builder.append(String.format("\n\t%d: Increase Balance", menuItem++));
-        builder.append(String.format("\n\t%d: Reset Player", menuItem++));
-        builder.append(String.format("\n\t%d: Quit\n", menuItem));
-        return console.getStringInput(builder.toString());
-    }
-
-    private Boolean keepTheCustomer() {
-        Boolean kept = true;
-        String answer = console.getStringInput("Can we get you a complimentary drink?");
-        switch (answer.toLowerCase()) {
-            case "n":
-            case "no":
-            case "no thankyou":
-            case "no thank you": {
-                kept = false;
-                break;
-            }
-            default:
-                break;
-        }
-        //more time wasting could be added here;
-        return kept;
-    }
-    void increaseBalance(){
-        Double amount = (console.getDoubleInput("You currently have $%,1.2f. How much would you like to add?",player.getWallet()));
-        Double confirmedAmt = confirmValidWalletIncrease(amount, player);
-        player.setWallet(player.getWallet()+confirmedAmt);
-    }
-
-    Double confirmValidWalletIncrease(Double amount, Player player) {
-        while(amount<=0||amount>999999999) {
-            if (amount <= 0) {
-                amount = console.getDoubleInput("I'm sorry, but we do not exchange negative or zero balances. Please enter a valid amount.");
-            }
-            else{
-                amount = console.getDoubleInput("I'm sorry, but we are not able to exchange billions in individual transactions. Please enter a reasonable amount.");
-            }
-        }
-        return amount;
     }
 }
--- src/test/java/com/groupFour/MediatorTest.java
+++ src/test/java/com/groupFour/MediatorTest.java
@@ -24,6 +24,49 @@ public class MediatorTest {
         Assert.assertEquals(expected,actual);
     }
 
+
+    @Test
+    public void initWalletTestStartNegative(){
+        Double invalid = -50.0;
+        Double expected =50.0;
+        String buffer = invalid+"\n"+expected;
+        testInputStream = new ByteArrayInputStream(buffer.getBytes());
+        console = new Console(testInputStream,System.out);
+        Player player = new Player("Player 1", null);
+        Mediator med = new Mediator(console);
+        med.initPlayerWallet(player);
+        Double actual = player.getWallet();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void initWalletTestStartBillion(){
+        Double invalid = 1000000000.0;
+        Double expected =50.0;
+        String buffer = invalid+"\n"+expected;
+        testInputStream = new ByteArrayInputStream(buffer.getBytes());
+        console = new Console(testInputStream,System.out);
+        Player player = new Player("Player 1", null);
+        Mediator med = new Mediator(console);
+        med.initPlayerWallet(player);
+        Double actual = player.getWallet();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void initWalletTestStartString(){
+        String invalid = "this isn't a number";
+        Double expected =50.0;
+        String buffer = invalid+"\n"+expected;
+        testInputStream = new ByteArrayInputStream(buffer.getBytes());
+        console = new Console(testInputStream,System.out);
+        Player player = new Player("Player 1", null);
+        Mediator med = new Mediator(console);
+        med.initPlayerWallet(player);
+        Double actual = player.getWallet();
+        Assert.assertEquals(expected,actual);
+    }
+
     @Test
     public void initPlayerTest(){
         String expected = "test data";
@@ -37,7 +80,7 @@ public class MediatorTest {
     }
 
     @Test
-    public void initAndBuildGamesMenu() {
+    public void initAndBuildGamesMenu() { // this just confirms that the build method doesn't throw any exceptions
         String expected = "test data";
         testInputStream = new ByteArrayInputStream(expected.getBytes());
         console = new Console(testInputStream,System.out);
@@ -53,8 +96,19 @@ public class MediatorTest {
         testInputStream = new ByteArrayInputStream(expected.toString().getBytes());
         console = new Console(testInputStream,System.out);
         Mediator med = new Mediator(console);
+        Player player = new Player("asdf",0.0);
+        Double actual = med.confirmValidWalletIncrease(expected,player);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void confirmValidWalletIncreaseStartNegative() {
+        Double expected =50.0;
+        testInputStream = new ByteArrayInputStream(expected.toString().getBytes());
+        console = new Console(testInputStream,System.out);
+        Mediator med = new Mediator(console);
         Double invalid=-1.0;
-        Player player = new Player("asdf",10.0);
+        Player player = new Player("asdf",0.0);
         Double actual = med.confirmValidWalletIncrease(invalid,player);
         Assert.assertEquals(expected,actual);
     }
@@ -66,6 +120,54 @@ public class MediatorTest {
         Double expected =initialWallet+increase;
         Player player = new Player("asdf",initialWallet);
         testInputStream = new ByteArrayInputStream(increase.toString().getBytes());
+        console = new Console(testInputStream,System.out);
+        Mediator med = new Mediator(console,player);
+        med.increaseBalance();
+        Double actual = player.getWallet();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void increaseBalanceStartNegative() {
+        Double initialWallet = 50.0;
+        Double invalidIncrease = -25.0;
+        Double increase = 25.0;
+        String buffer = invalidIncrease+"\n"+increase;
+        Double expected =initialWallet+increase;
+        Player player = new Player("asdf",initialWallet);
+        testInputStream = new ByteArrayInputStream(buffer.getBytes());
+        console = new Console(testInputStream,System.out);
+        Mediator med = new Mediator(console,player);
+        med.increaseBalance();
+        Double actual = player.getWallet();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void increaseBalanceStartBillion() {
+        Double initialWallet = 50.0;
+        Double invalidIncrease = 1000000000.0;
+        Double increase = 25.0;
+        String buffer = invalidIncrease+"\n"+increase;
+        Double expected =initialWallet+increase;
+        Player player = new Player("asdf",initialWallet);
+        testInputStream = new ByteArrayInputStream(buffer.getBytes());
+        console = new Console(testInputStream,System.out);
+        Mediator med = new Mediator(console,player);
+        med.increaseBalance();
+        Double actual = player.getWallet();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void increaseBalanceStartString() {
+        Double initialWallet = 50.0;
+        String invalidIncrease = "this isn't a number";
+        Double increase = 25.0;
+        String buffer = invalidIncrease+"\n"+increase;
+        Double expected =initialWallet+increase;
+        Player player = new Player("asdf",initialWallet);
+        testInputStream = new ByteArrayInputStream(buffer.getBytes());
         console = new Console(testInputStream,System.out);
         Mediator med = new Mediator(console,player);
         med.increaseBalance();
